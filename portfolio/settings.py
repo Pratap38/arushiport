b746f36504ddf7553210a0f78ed8dbe0daa2f1ee
ESME_PRATAP <pratapchoubey3@gmail.com>
2025-10-21 18:31:08 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -53,12 +53,12 @@ TEMPLATES = [
 ]
 
 # Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
